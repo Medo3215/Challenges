350f626ace0c710094009f6eefac6b1b673aff3f
MOHAMED ELIDRYSY <75158922+Medo3215@users.noreply.github.com>
2022-12-13 08:27:01 +0100

    Med ELIDIRYSY
    @themanoftalent

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,15 @@
 // your name surname and age from user
+#include<stdio.h>
+
+int main(){
+  char name;
+  char surname;
+  int age;
+  printf("Enter your name : ", name);
+  scanf("%s", &name);
+  printf("Enter your surname : ", surname);
+  scanf("%s", &surname);
+  printf("Enter your age : ", age);
+  scanf("%d", &age);
+
+}
